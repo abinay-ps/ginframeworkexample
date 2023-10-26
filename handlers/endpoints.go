c6b0bba084acb4020c9e6f3528b3191291d847c2
abinay-ps <ps.abinay@indiapost.gov.in>
2023-10-26 17:57:56 +0530

    small changes

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func callhandlers(router *gin.Engine) {
+func CallHandlers(router *gin.Engine) {
 	router.GET("/movie", functions.getMovies)             //Get all movies
 	router.GET("/movie/:id", functions.getMovieByID)      //Get a movie by its ID
 	router.POST("/movie", functions.createMovie)          // Create a new movie data
--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	router := gin.Default()
-
+	handlers.callhandlers(router)
 }
