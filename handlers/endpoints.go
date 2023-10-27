b9ba123635a8c8149b8ebff552ab0af6e969946f
abinay-ps <ps.abinay@indiapost.gov.in>
2023-10-27 09:55:18 +0530

    Edited handler functions

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -1,13 +1,14 @@
 package handlers
 
 import (
+	"github.com/abinay-ps/ginframeworkexample/functions"
 	"github.com/gin-gonic/gin"
 )
 
 func CallHandlers(router *gin.Engine) {
-	router.GET("/movie", functions.getMovies)             //Get all movies
-	router.GET("/movie/:id", functions.getMovieByID)      //Get a movie by its ID
-	router.POST("/movie", functions.createMovie)          // Create a new movie data
-	router.PATCH("movie/:id", functions.updateMoviePrice) //Update a particular movie price
-	router.DELETE("movie/:id", functions.deleteMovie)     //Delete a particular movie by its ID
+	router.GET("/movie", functions.GetMovies)             //Get all movies
+	router.GET("/movie/:id", functions.GetMovieByID)      //Get a movie by its ID
+	router.POST("/movie", functions.CreateMovie)          // Create a new movie data
+	router.PATCH("movie/:id", functions.UpdateMoviePrice) //Update a particular movie price
+	router.DELETE("movie/:id", functions.DeleteMovie)     //Delete a particular movie by its ID
 }
