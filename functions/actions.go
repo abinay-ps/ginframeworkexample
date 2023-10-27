c518817f3f3acc4cb3a7dc65ca61906d902dc771
abinay-ps <ps.abinay@indiapost.gov.in>
2023-10-27 10:21:48 +0530

    updating handler functions

--- functions/actions.go
+++ functions/actions.go
@@ -12,7 +12,14 @@ func GetMovies(c *gin.Context) {
 }
 
 func GetMovieByID(c *gin.Context) {
-	c.JSON(http.StatusOK, entity.Movies)
+	id := c.Param("id")
+	for _, val := range entity.Movies {
+		if id == val.ID {
+			c.JSON(http.StatusOK, val)
+			return
+		}
+	}
+	c.JSON(http.StatusOK, "No movie exists for the inputed ID")
 }
 
 func CreateMovie(c *gin.Context) {
